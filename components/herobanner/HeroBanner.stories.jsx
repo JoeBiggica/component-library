cd20d41c34fb12c66feda1ec4bd5e2bc21b91dec
Joe Biggica <joe.biggica@thomsonreuters.com>
2020-07-21 14:07:09 -0400

    Updated HeroBanner story

--- components/herobanner/HeroBanner.stories.jsx
+++ components/herobanner/HeroBanner.stories.jsx
@@ -18,7 +18,7 @@ storiesOf('component-library/HeroBanner', module)
 				title={text('title', 'Hero Banner Title')}
 				text_position={select('text_position', HeroBanner.TextPosition)}
 				text_gradient={select('text_gradient', HeroBanner.TextGradient)}
-				background_image='https://biggica-sites.s3.amazonaws.com/fongs-hung-ga/images/lion-dance/three-lions.jpg'
+				background_image={text('background_image', 'https://biggica-sites.s3.amazonaws.com/fongs-hung-ga/images/lion-dance/three-lions.jpg')}
 				background_position={select('background_position', HeroBanner.BackgroundPosition)}
 				background_gradient={select('background_gradient', HeroBanner.BackgroundGradient)}
 				hero_height={select('hero_height', HeroBanner.HeroHeight)}	
