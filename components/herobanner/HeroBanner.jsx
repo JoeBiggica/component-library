f5259d980bf21de89307bcffeb3cb6dc91a9f061
Joe Biggica <joe.biggica@thomsonreuters.com>
2020-07-21 14:00:09 -0400

    Added HeroBanner features

--- components/herobanner/HeroBanner.jsx
+++ components/herobanner/HeroBanner.jsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import TextLabel from 'components/textlabel';
+
 import styles from './HeroBanner.scss';
 
 const HeroHeight = {
@@ -12,9 +14,15 @@ const HeroHeight = {
 };
 
 const TextPosition = {
-	TOP: 'top',
 	BOTTOM: 'bottom',
+	TOP: 'top',
 	CENTER: 'center'
+}
+
+const TextGradient = {
+	BOTTOM: 'bottom',
+	TOP: 'top',
+	NONE: null	
 }
 
 const ButtonDirection = {
@@ -23,14 +31,15 @@ const ButtonDirection = {
 }
 
 const BackgroundPosition = {
-	TOP: 'top',
-	BOTTOM: 'bottom',
-	CENTER: 'center'
-}
-
-const Gradient = {
+	CENTER: 'center',
 	TOP: 'top',
 	BOTTOM: 'bottom'
+}
+
+const BackgroundGradient = {
+	NONE: null,
+	BOTTOM: 'bottom',
+	TOP: 'top'
 }
 
 class HeroBanner extends PureComponent {
@@ -40,6 +49,7 @@ class HeroBanner extends PureComponent {
 		title: PropTypes.string,
 		title_border: PropTypes.bool,
 		text_position: PropTypes.oneOf(Object.values(TextPosition)),
+		text_gradient: PropTypes.oneOf(Object.values(TextGradient)),
 		buttons: PropTypes.array,
 		button_border: PropTypes.bool,
 		button_direction: PropTypes.string,
@@ -47,38 +57,42 @@ class HeroBanner extends PureComponent {
 		background_image: PropTypes.string,
 		background_position: PropTypes.oneOf(Object.values(BackgroundPosition)),
 		hero_height: PropTypes.oneOf(Object.values(HeroHeight)),
-		gradient: PropTypes.oneOf(Object.values(Gradient)),
+		background_gradient: PropTypes.oneOf(Object.values(BackgroundGradient)),
 	}
 
 	static defaultProps = {
+		background_position: BackgroundPosition.CENTER,
+		text_position: TextPosition.BOTTOM,
+		text_gradient: TextGradient.BOTTOM,
 		button_direction: ButtonDirection.ROW,
-		hero_height: HeroHeight.FULL
+		hero_height: HeroHeight.FULL,
+		background_gradient: BackgroundGradient.NONE
 	}
 
 	static HeroHeight = HeroHeight
 	static TextPosition = TextPosition
+	static TextGradient = TextGradient
 	static ButtonDirection = ButtonDirection
 	static BackgroundPosition = BackgroundPosition
-	static Gradient = Gradient
-
-	getBackground = (background_image, gradient) => {
-		return `url(${background_image})`;
-	}
+	static BackgroundGradient = BackgroundGradient
 
 	renderButton = (button, index) => {
-		const button_container_classname = styles('button-container', {
-			'border': this.props.button_border,
-			'column': this.props.button_direction === 'column',
-			'row': this.props.button_direction === 'row',
-		});
+		const button_container_classname = classnames(
+			styles['button-container'],
+			styles[this.props.button_border && 'border'],
+			styles[this.props.button_direction === 'column' && 'column'],
+			styles[this.props.button_direction === 'row' && 'row'],
+		);
 
-		const button_classname = styles('button', {
-			'border': this.props.button_border,
-		});
+		const button_classname = classnames(
+			styles['button'],
+			styles[this.props.button_border && 'border']
+		);
+
 		return (	
 			<li className={button_container_classname}>
 				{button.label && 
-					<span className={styles('label')}>{button.label}</span>
+					<span className={classnames(styles['label'])}>{button.label}</span>
 				}
 				<div className={button_classname}>
 					<a href={button.url} target={button.target} onClick={button.onClick}>
@@ -95,49 +109,69 @@ class HeroBanner extends PureComponent {
 			className,
 			title,
 			title_border,
+			text_gradient,
 			text_position,
 			buttons,
 			button_border,
 			image,
 			background_image,
 			background_position,
-			hero_height,
-			gradient,
+			background_gradient,
+			hero_height
 		} = this.props;
 
-		const container_classname = styles('container', {
-			'justify-top': text_position === 'top',
-			'justify-bottom': text_position === 'bottom',
-			'full': hero_height === 'full',
-			'three-quater': hero_height === 'three-quater',
-			'half': hero_height === 'half',
-			'auto': hero_height === 'auto',
-		});
+		const container_classname = classnames(
+			className,
+			styles['container'],
+			styles[text_position === 'top' && 'justify-top'],
+			styles[text_position === 'bottom' && 'justify-bottom'],
+			styles[hero_height === 'full' && 'full'],
+			styles[hero_height === 'three-quater' && 'three-quater'],
+			styles[hero_height === 'half' && 'half'],
+			styles[hero_height === 'auto' && 'auto']
+		);
 
 		const background_style = {
-			backgroundImage: background_image && this.getBackground(background_image, gradient),
+			backgroundImage: background_image && `url(${background_image})`,
 			backgroundPosition: background_position && background_position,
 		};
 
-		const text_container_classname = styles('text-container', 'parallax-layer', 'parallax-base', {
-			'border': title_border,
-		});
+		const text_container_classname = classnames(
+			styles['text-container'],
+			styles[title_border && 'border'],
+			styles[text_gradient && `gradient-${text_gradient}`],
+		);
+
+		const background_gradient_classname = classnames(
+			styles['background-gradient'],
+			styles[background_gradient === 'top' && 'top'],
+			styles[background_gradient === 'bottom' && 'bottom']
+		);
 
 		return (
-			<div className={classnames(container_classname, className)}  style={background_style}>
-				<div className={text_container_classname}>
-					<h1 className={styles('title')}>{this.props.title}</h1>
-				</div>
-				{buttons &&
-					<ul className={styles('buttons-container')}>
+			<div className={container_classname}  style={background_style}>
+				{ title &&
+					<div className={text_container_classname}>
+						<TextLabel
+							className={classnames(styles['title'])}
+							tag='h1'
+							text={title}
+							color={TextLabel.Color.WHITE}
+							font={TextLabel.Font.AVENIR_BOLD}
+						/>
+					</div>
+				}
+				{ buttons &&
+					<ul className={classnames(styles['buttons-container'])}>
 						{buttons.map(this.renderButton)}
 					</ul>
 				}
-				{image &&
-					<div className={styles('image-container')} onClick={image.onClick}>
-						<img className={styles('image')} src={image.src} />
+				{ image &&
+					<div className={classnames(styles['image-container'])} onClick={image.onClick}>
+						<img className={classnames(styles['image'])} src={image.src} />
 					</div>
 				}
+				{ background_gradient && <div className={background_gradient_classname} /> }
 			</div>
 		);	
 	}
---
+++ components/herobanner/HeroBanner.stories.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, object, select, text, boolean } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered/react';
+import HeroBanner from './HeroBanner';
+
+const black_bg = {
+	name: 'black',
+	value: '#000',
+};
+
+storiesOf('component-library/HeroBanner', module)
+	.addDecorator(centered)
+	.addDecorator(withKnobs)
+	.add('default', () => (
+		<div style={{width: '100vw'}}>
+			<HeroBanner 
+				title={text('title', 'Hero Banner Title')}
+				text_position={select('text_position', HeroBanner.TextPosition)}
+				text_gradient={select('text_gradient', HeroBanner.TextGradient)}
+				background_image='https://biggica-sites.s3.amazonaws.com/fongs-hung-ga/images/lion-dance/three-lions.jpg'
+				background_position={select('background_position', HeroBanner.BackgroundPosition)}
+				background_gradient={select('background_gradient', HeroBanner.BackgroundGradient)}
+				hero_height={select('hero_height', HeroBanner.HeroHeight)}	
+			/>
+		</div>
+	), {
+		backgrounds: [black_bg]
+	});
